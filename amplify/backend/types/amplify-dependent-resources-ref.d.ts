fb271d94db4f89999ea6c45f99ccc8521addf9ce
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-03-05 20:39:47 -0600

    amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "notesapp22": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
