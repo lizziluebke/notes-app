dc8b36b11c4bca0bcc982219d13323b6c5b85c18
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-03-08 10:11:55 -0600

    ready  to start coding the react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useReducer } from 'react'; 
+import {  API } from 'aws-amplify';
+import {  list } from 'antd';
+import 'antd/dist/antd.css';
+import {  listNotes } from './graphql/queries'; 
+
 function App() {
   return (
     <div className="App">
