d53704d337965074ed133b1a3cc815cf0af5f636
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-03-12 17:09:22 -0600

    got adding new notes working

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 import React, { useEffect, useReducer } from 'react'; 
 import {  API } from 'aws-amplify';
-import {  List } from 'antd';
+import {  List, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 import {  listNotes } from './graphql/queries'; 
+import { v4 as uuid } from 'uuid';
+import { createNote as CreateNote } from './graphql/mutations';
+
+const CLIENT_ID = uuid();
 
 const initialState = {
   notes: [],
@@ -17,9 +21,20 @@ const initialState = {
 const reducer = (state, action) => {
   switch(action.type) {
     case 'SET_NOTES':
-      return { ...state, notes: action.notes, loading: false }
+      return { ...state, notes: action.notes, loading: false };
+
     case 'ERROR':
-      return { ...state, loading: false, error: true }
+      return { ...state, loading: false, error: true };
+
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]};
+
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
+
     default:
       return { ...state};
   }
@@ -49,6 +64,46 @@ const App = () => {
     fetchNotes()
   }, []);
 
+
+  const createNote = async () => {
+    const { form } = state
+    if (!form.name || !form.description) {
+       return alert('please enter a name and description');
+    }
+
+
+    const note = {
+       ...form, clientId: CLIENT_ID, completed: false, id: uuid() 
+    };
+
+    dispatch({
+       type: 'ADD_NOTE', note 
+    });
+    dispatch({ 
+      type: 'RESET_FORM' 
+    });
+
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { 
+          input: note 
+        }
+      });
+
+      console.log('successfully created note!');
+
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+
+  const onChange = (e) => {
+    dispatch({ 
+      type: 'SET_INPUT', name: e.target.name, value: e.target.value 
+    })
+  };
+
   const renderItem = (item) => {
     return (
       <List.Item 
@@ -64,7 +119,30 @@ const App = () => {
 
 
   return (
-    <div style={styles.container}>
+    <div style={styles.container}
+    >
+    <Input
+      onChange={onChange}
+      value={state.form.name}
+      placeholder="Note Name"
+      name='name'
+      style={styles.input}
+    />
+    <Input
+      onChange={onChange}
+      value={state.form.description}
+      placeholder="Note description"
+      name='description'
+      style={styles.input}
+    />
+    <Button
+      onClick={createNote}
+      type="primary"
+      >
+        Create Note
+    </Button>
+
+
       <List
         loading={state.loading}
         dataSource={state.notes}
